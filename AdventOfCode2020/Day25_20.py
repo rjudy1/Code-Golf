8a73774d0f043e1c5435fcb52fbf7b28c0f7440a
rjudy1 <64846855+rjudy1@users.noreply.github.com>
2020-12-25 13:12:28 -0500

    Day 25 2020!!

--- AdventOfCode2020/Day25_20.py
+++ AdventOfCode2020/Day25_20.py
@@ -1,8 +1,9 @@
 # Advent of Code 2020 - Day 25
 # Author:   Rachael Judy
 # Date:     12/25/2020
-# Purpose:  Find loop size and use with other public for privatek (15 sec for 3 lines, adding value var makes it 8 sec)
+# Purpose:  Do transform to find loop size; perform transform on other key with holding loop size (15s -add v to 1/2)
 
-inp, loop_size, value = [6269621, 8252394], 0, 0  # encryption keys, checks against value, using incrementing loop size
+# checks using incrementing loop_size - start at known option, transform till match, var inp is your input in array form
+loop_size, inp = 7**7, [6269621, 8252394]
 while pow(7, loop_size, 20201227) != inp[0] and pow(7, loop_size, 20201227) != inp[1]:  loop_size += 1
-print("private encryption key: ", pow(inp[pow(7, loop_size, 20201227)==inp[0]], loop_size, 20201227))  # other key, loop_size, mod num
+print("private key: ", pow(inp[pow(7, loop_size, 20201227)==inp[0]], loop_size, 20201227))  # other key, loop_size, mod
