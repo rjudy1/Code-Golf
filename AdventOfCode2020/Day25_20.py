19d6b3d42986c800a70881984777a2c2669e9826
rjudy1 <64846855+rjudy1@users.noreply.github.com>
2020-12-25 07:47:38 -0500

    Day 25 2020!!

--- AdventOfCode2020/Day25_20.py
+++ AdventOfCode2020/Day25_20.py
@@ -1,12 +1,8 @@
 # Advent of Code 2020 - Day 25
 # Author:   Rachael Judy
 # Date:     12/25/2020
-# Purpose:  Find loop size to generate public key and use loop size with other public key to find private key (RSAish)
+# Purpose:  Find loop size and use with other public for privatek (15 sec for 3 lines, adding value var makes it 8 sec)
 
-inp = [8252394, 6269621]  # encryption keys go here (your input)
-loop_size, value = 0, 0  # checks against value, using incrementing loop size
-while value != inp[0] and value != inp[1]:
-    loop_size += 1
-    value = pow(7, loop_size, 20201227)  # subject key, loop size, modulus (change consts if your stmt differs)
-
-print("private encryption key: ", pow(inp[value==inp[0]], loop_size, 20201227))  # other key, loop_size, mod num
+inp, loop_size, value = [6269621, 8252394], 0, 0  # encryption keys, checks against value, using incrementing loop size
+while pow(7, loop_size, 20201227) != inp[0] and pow(7, loop_size, 20201227) != inp[1]:  loop_size += 1
+print("private encryption key: ", pow(inp[pow(7, loop_size, 20201227)==inp[0]], loop_size, 20201227))  # other key, loop_size, mod num
