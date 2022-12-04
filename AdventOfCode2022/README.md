94da93dc794aeae701d34a45d5d9ec997e3e8d15
Rachael Judy <rjudy@cedarville.edu>
2022-12-04 08:24:54 -0500

    day 05

---
+++ AdventOfCode2022/README.md
@@ -0,0 +1,8 @@
+## Advent of Code 2022 in Python
+
+### Challenges
+1. Split a data file twice and find the top 3 sums of each batch
+2. Rock, paper, scissors strategy guide options
+3. Set intersection within row and between three rows
+4. Subset and intersection checks
+5. 
--- AdventOfCode2022/day05.py
+++ AdventOfCode2022/day05.py
@@ -1,12 +1,12 @@
-# Advent of Code 2022 Day 4
+# Advent of Code 2022 Day 5
 # Author:   Rachael Judy
-# Date:     12/4/22
-# Purpose:  Subset and intersection checks
+# Date:     12/5/22
+# Purpose:
 
 import parseMod
 
-ready = True
-day = 4
+ready = False
+day = 5
 stage = 'a'
 
 year = 2022
@@ -14,12 +14,11 @@ year = 2022
 parseMod.createDataFile(year=year, day=day)
 data = parseMod.readCSV_rowEl("data/" + str(day).zfill(2) + "data.csv", ',')
 
-pairs = [[set(range(int(e.split('-')[0]), int(e.split('-')[1]) + 1)) for e in row] for row in data]
 
 if stage == 'a':
-    result = sum(1 if p[0].issubset(p[1]) or p[1].issubset(p[0]) else 0 for p in pairs)
+    pass
 else:
-    result = sum(1 if len(p[0].intersection(p[1])) else 0 for p in pairs)
+    pass
 
 if not ready:
     print(f'result: \n{result}')
