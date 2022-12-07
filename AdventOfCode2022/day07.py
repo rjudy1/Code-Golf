dd81484063f3fb061684b0218f93b14fb1273301
Rachael Judy <rachael.judy@tangramflex.com>
2022-12-07 15:24:51 -0500

    slightly less clumsy day 07, but should still have tree class around file class probably

--- AdventOfCode2022/day07.py
+++ AdventOfCode2022/day07.py
@@ -1,92 +1,86 @@
 # Advent of Code 2022 Day 7
 # Author:   Rachael Judy
 # Date:     12/7/22
-# Purpose:  Determine file system from cd and ls commands (build tree),
+# Purpose:  Determine file system from cd and ls commands (build tree - slightly clumsy),
 #           sum sizes of directories less than 1000000,
 #           determine the smallest file needed to delete for update
 
 import parseMod
 
-stage = 'a'
+stage = 'b'
 day = 7
 year = 2022
-
-parseMod.createDataFile(year=year, day=day)
+parseMod.createDataFile(year=2022, day=7)
 commands = parseMod.readCSV_row("data/" + str(day).zfill(2) + "data.csv")
+commands.append('$ cd /')  # add this to always go back up the top file at end
 index = 0
 
 
 class File:
-    def __init__(self, name='', parent=None, is_dir=False, size=0):
+    def __init__(self, name='/', parent=None, size=0):
         self.name = name
         self.parent = parent
-        self.is_dir = is_dir
         self.size = size
         self.children = {}  # names to children trees
 
-    def add_child(self, new_tree):
-        self.children[new_tree.name] = new_tree
+    def find_folder_of_min_size(self, minimum_size):
+        remove_size = self.size if self.size > minimum_size and len(self.children) else 1000000000
+        for child in self.children:
+            remove_size = min(remove_size, self.children[child].find_folder_of_min_size(minimum_size))
+        return remove_size
 
 
-def construct_tree(commands, file):
+def update_sizes(tree):
+    for child in tree.children:
+        tree.size += update_sizes(tree.children[child]).size
+    return tree
+
+
+def build_from_commands(commands, file=None):
     global index
     while index < len(commands):
         c = commands[index].split(' ')
         if c[1] == 'ls':
-            index += 1
-
-            while index < len(commands) and commands[index].split(' ')[0] != '$':
-                c = commands[index].split(' ')
-                if c[0] == 'dir':
-                    file.add_child(File(c[1], file, True, 0))
-                else:
-                    file.add_child(File(c[1], file, False, int(c[0])))
-                index += 1
-
+            pass
         elif c[1] == 'cd':
             if c[2] == '/':
-                # only appears at top so going to cheat a little bit
-                file = File('/', None, True)
+                if file is None:
+                    file = File('/', None, True)
+                else:
+                    while file.parent is not None:
+                        file = file.parent
             elif c[2] == '..':
                 # go back up the tree (return current state ig)
                 if file.parent is not None:
                     return file.parent
             else:
-                file.add_child(File(c[2], file, True, 0))
+                file.children[c[2]] = File(c[2], file)
                 index += 1
-                file = construct_tree(commands, file.children[c[2]])
-            index += 1
+                file = build_from_commands(commands, file.children[c[2]])
+        else:
+            file.children[c[1]] = File(c[1], file, int(c[0]) if c[0] != 'dir' else 0)
 
-    while file.parent is not None:
-        file = file.parent
+        index += 1
     return file
 
 
-def dfs_sum(tree):
-    for child in tree.children:
-        tree.size += dfs_sum(tree.children[child])[1]
-    return tree, tree.size
+def construct_tree(commands, file=None):
+    tree = build_from_commands(commands)
+    return update_sizes(tree)
 
 
 def solve_a(tree, maximum_size):
-    tree_size = tree.size if tree.size < maximum_size and tree.is_dir else 0
+    tree_size = tree.size if tree.size < maximum_size and len(tree.children) else 0
     return tree_size + sum(solve_a(tree.children[child], maximum_size) for child in tree.children)
 
 
-def solve_b(tree, minimum_size):
-    min_size = tree.size if tree.size > minimum_size and tree.is_dir else 1000000000
-    for child in tree.children:
-        min_size = min(min_size, solve_b(tree.children[child], minimum_size))
-    return min_size
-
 # construct tree
-tree = construct_tree(commands, None)
-tree, size = dfs_sum(tree)
+tree = construct_tree(commands)
 
 if stage == 'a':
     result = solve_a(tree, 100000)
 else:
-    result = solve_b(tree, min(30000000, 30000000 - 70000000 + tree.size))
+    result = tree.find_folder_of_min_size(30000000 - 70000000 + tree.size)
 
 print("SUBMITTING RESULT: ", result)
 parseMod.submit(result, part=stage, day=day, year=year)
