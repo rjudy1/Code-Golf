2bc715cc6afb82ac022e5e5c4f7845dc12966d0e
Rachael Judy <rjudy@cedarville.edu>
2022-12-18 16:21:51 -0500

    day17 working cleaned up

--- AdventOfCode2022/day17.py
+++ AdventOfCode2022/day17.py
@@ -1,7 +1,7 @@
 # Advent of Code 2022 Day 17
 # Author:   Rachael Judy
 # Date:     12/17/22
-# Purpose:  tetris with compute where repetition starts (length jets * blocks?)
+# Purpose:  straight simulate tetris placement until repetition with bfs hash, then finish after repeats
 
 import parseMod
 import time
@@ -18,53 +18,52 @@ blocks = [((0, 0), (1, 0), (2, 0), (3, 0)),
           ((0, 0), (1, 0), (2, 0), (2, 1), (2, 2)),
           ((0, 0), (0, 1), (0, 2), (0, 3)),
           ((0, 0), (0, 1), (1, 0), (1, 1))]
-open = lambda x, y, occupied: 0 <= x < 7 and y >= 0 and (x, y) not in occupied
+is_open = lambda x, y, occupied: 0 <= x < 7 and y >= 0 and (x, y) not in occupied
 
 
-def place(occupied, jet, piece, max_height):
-    def can_move(piece_i, x, y):
-        return all(open(x+dx, y+dy, occupied) for dx, dy in blocks[piece_i])
-
+def place_block(jet, piece, max_height, occupied):
     x, y = 2, max_height + 4
+    can_move = lambda piece_i, x, y: all(is_open(x + dx, y + dy, occupied) for dx, dy in blocks[piece_i])
     while True:
-        if can_move(piece, x+jets[jet], y): x += jets[jet]
+        # jet fueled movement and vertical drop if possible
+        x += jets[jet] if can_move(piece, x+jets[jet], y) else 0
         jet = (jet + 1) % len(jets)
         if can_move(piece, x, y-1):
             y -= 1
         else:
             occupied.update([(x+dx, y+dy) for (dx, dy) in blocks[piece]])
             break
-    return jet%len(jets), max(max_height, max(y for _, y in [(x+dx, y+dy) for (dx, dy) in blocks[piece]]))
+    # return new position in jets and max_height to save running through old data
+    return jet % len(jets), max(max_height, max(y for _, y in [(x+dx, y+dy) for (dx, dy) in blocks[piece]]))
 
 
-def hash_view(occupied, maxy):
-    def dfs(x, y, visited):
-        # if space is not open or visited already or larger than worth hashing
-        if not open(x, maxy+y, occupied) or (x, y) in visited or len(visited) > 1: return
-        visited.add((x, y))
-        for nx, ny in ((x-1, y), (x+1, y), (x, y-1)):
-            dfs(nx, ny, visited)
-    state = set()
-    for x in range(7):
-        dfs(x, 0, state)
-    return tuple(state) if len(state) <= 1 else None
+def hash_view(occupied, maxy, max_hash_size=25):
+    exp, ind = [(0, 0)], 0
+    while ind != len(exp):
+        for p in ((exp[ind][0], exp[ind][1]-1), (exp[ind][0]-1, exp[ind][1]), (exp[ind][0]+1, exp[ind][1])):
+            if 0 <= p[0] < 7 and p[1]+maxy+1 >= 0 and (p[0], maxy+1+p[1]) not in occupied and (p[0], p[1]) not in exp:
+                exp.append(p)
+        if len(exp) > max_hash_size: break
+        ind += 1
+    return tuple(exp) if len(exp) <= max_hash_size else None
 
 
 def solve(count):
-    T, cycles = set(), dict()
+    occupied, cycles = set(), dict()
     jet, maxy, piece, additional = 0, -1, 0, 0
-
     while count > 0:
-        jet, maxy = place(T, jet, piece % len(blocks), maxy)
+        # simulate
+        jet, maxy = place_block(jet, piece % len(blocks), maxy, occupied)
         count -= 1
         piece = (piece + 1) % len(blocks)
 
-        ground = hash_view(T, maxy)
+        # hash and cache
+        ground = hash_view(occupied, maxy, 12)  # must at least hash 7 around peak for my input
         if ground is None: continue
-        if (jet, piece, ground) in cycles:
-            old_maxy, oldcount = cycles[jet, piece, ground]
-            additional += (maxy - old_maxy) * (count // (oldcount - count))
-            count %= oldcount - count
+        elif (jet, piece, ground) in cycles:
+            old_maxy, old_count = cycles[jet, piece, ground]
+            additional += (maxy - old_maxy) * (count // (old_count - count))  # height increase caught in cycles
+            count %= cycles[jet, piece, ground][1] - count  # takes to end of this cycle as we're counting down
         cycles[jet, piece, ground] = (maxy, count)
 
     return maxy + additional
