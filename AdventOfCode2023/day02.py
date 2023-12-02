33a81e72a6935bb207372739078aa59c6e2a98f0
Rachael Judy <rjudy@cedarville.edu>
2023-12-02 12:40:58 -0500

    day2

--- AdventOfCode2023/day02.py
+++ AdventOfCode2023/day02.py
@@ -14,7 +14,30 @@ year = 2023
 parseMod.createDataFile(year=year, day=day)
 array = parseMod.readCSV_row("data/" + str(day).zfill(2) + "data.csv")
 
-
+result = 0
+for num, game in enumerate(array):
+    if stage == 'a':
+        possible = True
+        for draw in game.split(':')[1].split(';'):
+            for color in draw.split(','):
+                if color.strip().split(' ')[1] == 'red' and int(color.strip().split(' ')[0]) > 12 or \
+                   color.strip().split(' ')[1] == 'green' and int(color.strip().split(' ')[0]) > 13 or \
+                   color.strip().split(' ')[1] == 'blue' and int(color.strip().split(' ')[0]) > 14:
+                    possible = False
+                    break
+        if possible:
+            result += num+1
+    else:
+        red_max, green_max, blue_max = 0, 0, 0
+        for draw in game.split(':')[1].split(';'):
+            for color in draw.split(','):
+                if color.strip().split(' ')[1] == 'red':
+                    red_max = max(red_max, int(color.strip().split(' ')[0]))
+                elif color.strip().split(' ')[1] == 'green':
+                    green_max = max(green_max, int(color.strip().split(' ')[0]))
+                elif color.strip().split(' ')[1] == 'blue':
+                    blue_max = max(blue_max, int(color.strip().split(' ')[0]))
+        result += red_max * green_max * blue_max
 
 if not ready:
     print(f'result: \n{result}')
