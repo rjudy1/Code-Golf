ea60ddf4c09774bc83be764eb3db9fdb9db1e5ac
Rachael Judy <rjudy@cedarville.edu>
2024-12-10 14:05:29 -0500

    day10 done

--- AdventOfCode2024/day10.py
+++ AdventOfCode2024/day10.py
@@ -25,47 +25,18 @@ with open("data/" + str(day).zfill(2) + "data.csv") as file:
             if col == '0':  # grab the trailheads
                 queue.append((complex(i, j), complex(i, j)))
 
-import time
-start = time.time()
-# .013, .007
-# .008, .015
+# bfs from each trailhead, counting endpoints from each source/trails from each source
 result = 0
-if stage == 'a':
-    visited = collections.defaultdict(set)
-elif stage == 'b':
-    visited = collections.defaultdict(int)
-    for val in queue:
-        visited[val[0]] = 1
-    # queue = collections.deque([node for node,source in queue])
+visited = set()
 directions = [1, 1j, -1, -1j]
 while queue:
     node, source = queue.popleft()
     for dir in directions:
-        if node+dir in trail_map and trail_map[node] + 1 == trail_map[node+dir]: # and (stage == 'b' or (node+dir, source) not in visited):
-            if stage == 'a':
-                visited[node+dir].add(source)
-                queue.append((node + dir,source))
-            else:
-                if visited[node + dir] == 0:
-                    queue.append((node+dir, source))
-                visited[node+dir] += visited[node]
-for node in visited:
-    if trail_map[node] == 9:
-        result += visited[node] if stage == 'b' else len(visited[node])
-
-# bfs from each trailhead, counting endpoints from each source/trails from each source
-# result = 0
-# visited = set()
-# directions = [1, 1j, -1, -1j]
-# while queue:
-#     node, source = queue.popleft()
-#     for dir in directions:
-#         if node+dir in trail_map and trail_map[node] + 1 == trail_map[node+dir] and (stage == 'b' or (node+dir, source) not in visited):
-#             queue.append((node+dir, source))
-#             visited.add((node+dir, source))
-#             if trail_map[node+dir] == 9:
-#                 result += 1
-print(time.time()-start)
+        if node+dir in trail_map and trail_map[node] + 1 == trail_map[node+dir] and (stage == 'b' or (node+dir, source) not in visited):
+            queue.append((node+dir, source))
+            visited.add((node+dir, source))
+            if trail_map[node+dir] == 9:
+                result += 1
 
 if not ready:
     print(f'result: \n{result}')
