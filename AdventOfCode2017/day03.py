c75fb186e70c6bd02f5f6031820cbaff906e0655
Rachael Judy <rjudy@cedarville.edu>
2025-10-22 16:51:53 -0400

    partial 2017 day 3, switch machines needed

--- AdventOfCode2017/day03.py
+++ AdventOfCode2017/day03.py
@@ -1,8 +1,8 @@
-# Advent of Code 2023 Day 1
+# Advent of Code 2017 Day 3
 # Author:   Rachael Judy
-# Purpose:  sum all values that immediately match the next digit
+# Purpose:  use incrementing spiral and sum of existing neighbors spiral to find distance to value and value > input
 
-import numpy as np
+from collections import defaultdict
 
 import parseMod
 
@@ -11,12 +11,25 @@ day = 3
 stage = 'a'
 year = 2017
 
+import math
+
 parseMod.createDataFile(year=year, day=day)
 data = parseMod.readCSVInts("data/" + str(day).zfill(2) + "data.csv")[0]
 print(data)
 
+# no code needed for today but we'll write some for reference
+# note sequence of odd squares from bottom right corner and just walk around the perimeter
+# find nearest higher odd
+lower_right_root = math.ceil(math.sqrt(data)) + int(math.ceil(math.sqrt(data)) % 2 == 0)
+# figure out which side of the square the value is in and then distance from the nearest center
+# lower left is lower_right^2 - lower right_root +1
+print(lower_right_root)
 
-
+# for b, just build with existing, moving in spiral pattern
+visited = defaultdict(int)
+visited[(0, 0)] = 1
+# move in spiral pattern with break off too right, sum neighbors
+sum(visited[x+dx,y+dy] for (dx, dy) in [(-1,-1), (-1, 0), (-1, 1), (0,-1), (0,1), (1, -1), (1,0), (1,1)])
 
 if not ready:
     print(f'result: \n{result}')
