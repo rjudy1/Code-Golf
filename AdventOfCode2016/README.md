c3b2cbcdb6d1ebb7f4b6ebbb102479d0355eee57
Rachael Judy <rjudy@cedarville.edu>
2025-11-19 11:16:01 -0500

    day 5 2016

--- AdventOfCode2016/README.md
+++ AdventOfCode2016/README.md
@@ -4,4 +4,5 @@
 1. follow turn and move instructions, tracking visited
 2. follow step directions within bounds of a keypad
 3. triangle inequality checks
-4. 
+4. parse most frequent letters, then caesar cipher shift by id (exactly what phrase to search for could be more clear)
+5. 
---
+++ AdventOfCode2016/day04.py
@@ -0,0 +1,33 @@
+# Advent of Code 2016 Day 4
+# Author:   Rachael Judy
+# Purpose:  parse most frequent letters, then caesar cipher shift by id
+
+from collections import Counter
+import re
+
+import parseMod
+
+ready = True
+day = 4
+stage = 'b'
+year = 2016
+
+parseMod.createDataFile(year=year, day=day)
+data = parseMod.readCSV_row("data/" + str(day).zfill(2) + "data.csv")
+
+pattern = re.compile(r'((?:[a-z]+-)+)([0-9]+)\[([a-z]{5})\]')
+result = 0
+for line in data:
+    m = pattern.fullmatch(line)
+    lc, id, checksum = Counter(sorted(ls:=m.group(1).replace('-', ''))), int(m.group(2)), m.group(3)
+    if ''.join(letter for letter, _ in lc.most_common(5)) == checksum:
+        result += id
+        if stage == 'b' and 'northpole' in ''.join(chr(ord('a') + (ord(l)-ord('a')+id)%26) for l in ls):
+            result = id
+            break
+
+if not ready:
+    print(f'result: \n{result}')
+elif ready:
+    print("SUBMITTING RESULT: ", result)
+    parseMod.submit(result, part=stage, day=day, year=year)
